506b4734888a5f9571a22a76ba91cb7ed3433e1f
Wagner Loch <wagner_loch@hotmail.com>
2025-08-19 10:53:03 -0300

    Add saudacao function

--- Main.js
+++ Main.js
@@ -5,3 +5,12 @@ console.log("Olá mundo 2!");
 for (let i = 0; i < 5; i++) {
     console.log("Olá mundo " + i + "!");
 }
+
+function saudacaoPessoal(nome) {
+    console.log("Olá " + nome + "!");
+    console.log("Bem-vindo(a) ao nosso programa!");
+}
+
+saudacaoPessoal("Maria");
+saudacaoPessoal("João");
+saudacaoPessoal("Ana");
